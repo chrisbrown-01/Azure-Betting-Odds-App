7ce1285ca6d49190c4d283ca01f9e45f4551a6e5
chrisbrown-01 <chrisbrown1@hotmail.ca>
2023-08-21 02:01:37 -0400

    update readme, implement playwright tests

---
+++ BettingOddsCalculatorTests_Playwright/IndexTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingOddsCalculatorTests_Playwright
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class IndexTests : PageTest
+    {
+        private const string URL = "https://betting-odds-app.azurewebsites.net/";
+
+        [Test]
+        public async Task TestTitle()
+        {
+            await Page.GotoAsync(URL);
+            await Expect(Page).ToHaveTitleAsync("Home - Betting Odds Calculator");
+        }
+
+        [Test]
+        public async Task TestHeading()
+        {
+            await Page.GotoAsync(URL);
+            var heading = await Page.TextContentAsync("h1");
+            Assert.That(heading, Is.EqualTo("Betting Odds Calculator"));
+        }
+
+        [Test]
+        public async Task TestFormSubmission()
+        {
+            await Page.GotoAsync(URL);
+            await Page.FillAsync("#AmericanOdds", "100");
+            await Page.FillAsync("#BetAmount", "100");
+            await Page.ClickAsync("input[type=submit]");
+            var winnings = await Page.TextContentAsync("p:has-text(\"Winnings:\")");
+            var totalPayout = await Page.TextContentAsync("p:has-text(\"Total Payout:\")");
+            Assert.That(winnings, Is.EqualTo("Winnings: $100.00"));
+            Assert.That(totalPayout, Is.EqualTo("Total Payout: $200.00"));
+        }
+
+        [Test]
+        public async Task TestFormValidation()
+        {
+            await Page.GotoAsync(URL);
+            await Page.FillAsync("#AmericanOdds", "");
+            await Page.FillAsync("#BetAmount", "");
+            await Page.ClickAsync("input[type=submit]");
+            var americanOddsValidation = await Page.TextContentAsync("span[data-valmsg-for=\"AmericanOdds\"]");
+            var betAmountValidation = await Page.TextContentAsync("span[data-valmsg-for=\"BetAmount\"]");
+            Assert.That(americanOddsValidation, Is.EqualTo("The value '' is invalid."));
+            Assert.That(betAmountValidation, Is.EqualTo("The value '' is invalid."));
+        }
+
+        [Test]
+        public async Task TestNegativeOdds()
+        {
+            await Page.GotoAsync(URL);
+            await Page.FillAsync("#AmericanOdds", "-200");
+            await Page.FillAsync("#BetAmount", "100");
+            await Page.ClickAsync("input[type=submit]");
+            var winnings = await Page.TextContentAsync("p:has-text(\"Winnings:\")");
+            var totalPayout = await Page.TextContentAsync("p:has-text(\"Total Payout:\")");
+            Assert.That(winnings, Is.EqualTo("Winnings: $50.00"));
+            Assert.That(totalPayout, Is.EqualTo("Total Payout: $150.00"));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,6 +10,6 @@
 -ASP.NET Core Razor pages project hosted with Azure App Service
 -Retrieves and caches MMA betting lines from https://the-odds-api.com/ API
 -CI/CD pipeline with automatic deployments to Azure for the web app and Azure Function
--xUnit tests with CI/CD pipeline to auto-run tests upon new commits
+-xUnit and Playwright tests with CI/CD pipeline to auto-run tests upon new commits
 -app config variables set via Azure configuration settings
 
